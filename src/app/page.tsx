b77311bd06f5ab3beff8b44a690e5285eaeb60e6
Swapnil Sahoo <swapnil.sahoo@gmail.com>
2026-08-28 08:10:21 +0000

    Automated alert: homepage overloaded by high request volume

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,1 +1,7 @@
-export default function Home() { return <div style={{display:"flex",justifyContent:"center",alignItems:"center",height:"100vh",fontFamily:"sans-serif"}}><h1>503 Service Unavailable: Homepage Overloaded</h1></div>; }
+export default function Home() {
+  return (
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh", fontFamily: "sans-serif" }}>
+      <h1>503 Service Unavailable: Homepage Overloaded</h1>
+    </div>
+  );
+}
